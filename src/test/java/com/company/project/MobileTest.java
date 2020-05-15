34fb03520f40eeaafb6e5ce8e532c3ac84600d1f
apoorva deshpande <dapoorva18@gmail.com>
2020-05-15 15:23:48 -0400

    first commit

---
+++ src/test/java/com/company/project/MobileTest.java
@@ -0,0 +1,16 @@
+package com.company.project;
+
+import org.testng.annotations.Test;
+
+public class MobileTest {
+@Test
+public void browser()
+{
+System.out.println("mobile browser");	
+}
+@Test
+public void login()
+{
+System.out.println("mobile login");	
+}
+}
---
+++ src/test/java/com/company/project/WebTest.java
@@ -0,0 +1,17 @@
+package com.company.project;
+
+import org.testng.annotations.Test;
+
+public class WebTest {
+	@Test
+	public void browser()
+	{
+		System.out.println("web broser");
+	}
+	@Test
+	public void login()
+	{
+		System.out.println("web login");
+	}
+
+}
